49b2d27906fc13557e2b9e5e8ca9de58267325ea
romanmaroder <kashik.roman@rambler.ru>
2025-06-26 11:21:20 +0300

    Middleware 'auth' to client and user routs

--- routes/client.php
+++ routes/client.php
@@ -2,11 +2,15 @@
 
 use App\Http\Controllers\Client\ClientController;
 
-Route::put('clients/restore/{id}',[ClientController::class,'restore'])->name('clients.restore');
-Route::put('clients/multiRestore/{ids}',[ClientController::class,'multiRestore'])->name('multiRestore');
-Route::put('/avatar/{client}',[ClientController::class,'avatar'])->name('avatar');
+Route::middleware(['auth'])->group(function () {
+    Route::put('clients/restore/{id}',[ClientController::class,'restore'])->name('clients.restore');
+    Route::put('clients/multiRestore/{ids}',[ClientController::class,'multiRestore'])->name('multiRestore');
+    Route::put('/avatar/{client}',[ClientController::class,'avatar'])->name('avatar');
+    Route::resource('clients', ClientController::class);
+    Route::delete('clients/trash/{ids}', [ClientController::class, 'trash'])->name('trash');
+    Route::delete('clients/multiDestroy/{ids}', [ClientController::class, 'multiDestroy'])->name('multiDestroy');
+    Route::get('/archive',[ClientController::class,'archive'])->name('clients.archive');
+});
 
-Route::resource('clients', ClientController::class);
-Route::delete('clients/trash/{ids}', [ClientController::class, 'trash'])->name('trash');
-Route::delete('clients/multiDestroy/{ids}', [ClientController::class, 'multiDestroy'])->name('multiDestroy');
-Route::get('/archive',[ClientController::class,'archive'])->name('clients.archive');
+
+
