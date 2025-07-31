f09da5c837b49a91b22270f8f634133d4a9c2425
romanmaroder <kashik.roman@rambler.ru>
2025-07-31 14:09:55 +0300

    Drawer header - padding and title- font size

---
+++ resources/js/components/client/ClientTable.vue
@@ -0,0 +1,440 @@
+<script setup lang="ts">
+import CreateDialog from '@/components/client/CreateDialog.vue';
+import CreateForm from '@/components/client/CreateForm.vue';
+import DeleteDialog from '@/components/client/DeleteDialog.vue';
+import MultiDeleteDialog from '@/components/client/MultiDeleteDialog.vue';
+import UpdateDialog from '@/components/client/UpdateDialog.vue';
+//import UpdateDialog from '../../pages/client/Edit.vue';
+import Filter from '@/components/filters/user/Filter.vue';
+import { FilterMatchMode } from '@primevue/core/api';
+import Button from 'primevue/button';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import Drawer from 'primevue/drawer';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+import Tag from 'primevue/tag';
+import Toolbar from 'primevue/toolbar';
+import { onMounted, onUpdated, ref } from 'vue';
+
+const props = defineProps({
+    entities: {
+        type: Object,
+        required: true,
+        default() {
+            return {};
+        },
+    },
+    columns: {
+        type: Object,
+    },
+    filtersFields: {
+        type: Object,
+    },
+    tools: {
+        type: Object,
+        default() {
+            return {
+                create: false,
+                update: false,
+                restore: false,
+                remove: false,
+                multiRestore: false,
+                multiDelete: false,
+            };
+        },
+    },
+    routes: {
+        type: Object,
+        default() {
+            return {};
+        },
+    },
+});
+
+const emit = defineEmits(['count']);
+
+const dt = ref();
+const items = ref();
+const count = ref(0);
+const selectedItems = ref();
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+const fields = ref([]);
+
+const loading = ref(true);
+const pagination = ref(false);
+const visible = ref(false);
+
+onMounted(() => {
+    items.value = props.entities.data;
+    if (items.value.length > 0) {
+        pagination.value = true;
+    }
+    loading.value = false;
+
+    filterFields();
+});
+
+onUpdated(() => {
+    emit('count', (count.value = items.value.length));
+    if (items.value.length == 0) {
+        pagination.value = false;
+    }
+});
+
+const onDeleteItem = (id: any) => {
+    operationWithSingleItem(id);
+};
+const onLoadItem = () => {
+    items.value = props.entities.data;
+};
+
+const onDeleteSelectedItems = () => {
+    operationWithSelectedItems();
+};
+
+const getStatusLabel = (status: any) => {
+    switch (status) {
+        case true:
+            return 'success';
+        case false:
+            return 'warn';
+        default:
+            return null;
+    }
+};
+
+const filterFields = () => {
+    if (props.filtersFields) {
+        Object.entries(props.filtersFields).forEach(([key1, value1]) => {
+            Object.entries(value1).forEach(([key2, value2]) => {
+                if (key2 === 'field') {
+                    fields.value.push(value2);
+                }
+            });
+        });
+    } else {
+        Object.entries(props.columns).forEach(([key1, value1]) => {
+            Object.entries(value1).forEach(([key2, value2]) => {
+                if (key2 === 'field') {
+                    fields.value.push(value2);
+                }
+            });
+        });
+    }
+};
+
+const operationWithSelectedItems = () => {
+    items.value = items.value.filter((val: any) => !selectedItems.value.includes(val));
+    selectedItems.value = null;
+};
+
+const operationWithSingleItem = (id: any) => {
+    items.value = items.value.filter((val: any) => val.id !== id);
+};
+
+const trimPhone = (phoneNumber: string) => {
+    return phoneNumber.replace(/[- )(]/g, '');
+};
+</script>
+
+<template>
+    <div class="grid auto-cols-fr">
+        <Toolbar class="mb-6">
+            <template #start>
+                <div class="flex flex-row items-start space-x-2">
+                    <span class="sm:hidden">
+                            <CreateForm
+                                v-if="tools.create"
+                                icon-name="UserRoundPlus"
+                                label="New"
+                                title="New client"
+                                :route="routes.create"
+                                @create-item="onLoadItem"
+                            />
+                    </span>
+                    <span class="hidden sm:table-cell">
+                        <CreateDialog v-if="tools.create" icon-name="UserRoundPlus" label="New" title="New client" :route="routes.create" />
+                    </span>
+                    <span class="hidden sm:table-cell">
+                        <MultiDeleteDialog
+                            :entity="selectedItems"
+                            icon-name=""
+                            :route="routes.multiDestroy"
+                            :disabled="!selectedItems || !selectedItems.length"
+                            @delete-items="onDeleteSelectedItems"
+                        />
+                    </span>
+                </div>
+            </template>
+            <template #end>
+                <Drawer v-model:visible="visible" header="Filter" :pt="{
+                    header:{
+                        class:'!py-[0.5rem]'
+                    },
+                    title:{
+                        class:'!text-lg'
+                    }
+                }">
+                    <Filter :entities="entities" :route="{ index: 'clients.index' }" class-name="grid items-end gap-5 mt-2" />
+                </Drawer>
+                <Button icon="pi pi-search" @click="visible = true" size="small" />
+            </template>
+        </Toolbar>
+
+        <DataTable
+            class="text-[15px]"
+            ref="dt"
+            v-model:selection="selectedItems"
+            :value="items"
+            dataKey="id"
+            :paginator="pagination"
+            :rows="10"
+            :filters="filters"
+            :globalFilterFields="fields"
+            sortMode="multiple"
+            removable-sort
+            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+            :rowsPerPageOptions="[5, 10, 15, 20, 25]"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} items"
+            :loading="loading"
+        >
+            <template #header>
+                <div class="flex flex-wrap items-center justify-between gap-2">
+                    <h4 class="m-0">Manage clients</h4>
+                    <IconField>
+                        <InputIcon>
+                            <i class="pi pi-search" />
+                        </InputIcon>
+                        <InputText v-model="filters['global'].value" name="search" class="h-[28px]" placeholder="Search..." size="small" />
+                    </IconField>
+                </div>
+            </template>
+            <template #empty><p class="text-center text-xl font-bold">No entities</p></template>
+            <template #loading> Loading items data. Please wait.</template>
+            <Column
+                selectionMode="multiple"
+                :exportable="false"
+                :pt="{
+                    root: {
+                        class: 'hidden sm:table-cell'
+                    },
+                    pcRowCheckbox: {
+                        input: {
+                            name: 'selectedItem'
+                        }
+                    },
+                    pcHeaderCheckbox: {
+                        input: { name: 'allSelected' }
+                    }
+                }"
+            ></Column>
+            <Column
+                field="avatar"
+                header="Avatar"
+                :pt="{
+                    root: {
+                        class: 'hidden lg:table-cell'
+                    }
+                }"
+            >
+                <template #body="slotProps">
+                    <img v-if="slotProps.data.avatar" :src="slotProps.data.avatar" :alt="slotProps.data.avatar" class="max-w-[48px] rounded" />
+                    <img v-else class="max-w-[48px] rounded" src="../../../../public/no_avatar_big.png" alt="" />
+                </template>
+            </Column>
+            <Column field="name" header="Name" :sortable="true">
+                <template #body="slotProps">
+                    <div class="text-sm font-medium text-wrap text-gray-900 dark:text-white">
+                        <p>
+                            {{ slotProps.data.name }}
+                            {{ slotProps.data.middleName }}
+                            {{ slotProps.data.surname }}
+                        </p>
+                    </div>
+                    <p>
+                        <small class="text-xs font-normal text-gray-900 dark:text-gray-300">ID: {{ slotProps.data.id }}</small>
+                    </p>
+                    <p class="hidden sm:table-cell">
+                        <small class="text-xs font-normal text-gray-900 dark:text-gray-300">{{ slotProps.data.created_at }}</small>
+                    </p>
+                </template>
+            </Column>
+            <Column
+                field="phone"
+                header="Info"
+                :sortable="true"
+                :pt="{
+                    root: {
+                        class: 'sm:hidden'
+                    }
+                }"
+            >
+                <template #body="slotProps">
+                    <div class="text-sm font-medium text-wrap text-gray-900 dark:text-white">
+                        <Button
+                            class="!px-0"
+                            as="a"
+                            variant="link"
+                            :label="slotProps.data.phone"
+                            :href="'tel:' + trimPhone(slotProps.data.phone)"
+                            rel="noopener"
+                        />
+                    </div>
+                    <small class="text-xs font-normal text-gray-900 dark:text-gray-300">{{ slotProps.data.email }} </small>
+                    <p class="sm:hidden">
+                        <small class="text-xs font-normal text-gray-900 dark:text-gray-300">{{ slotProps.data.created_at }}</small>
+                    </p>
+                    <div class="mt-3 flex items-center justify-between text-xs font-normal text-gray-900 dark:text-gray-300">
+                        <UpdateDialog
+                            :key="slotProps.data.id"
+                            v-if="tools.update"
+                            :entity="slotProps.data"
+                            icon-name="Edit2"
+                            label="Edit item"
+                            :route="routes.update"
+                            @update-item="onLoadItem"
+                        />
+                        <DeleteDialog
+                            v-if="tools.remove"
+                            :entity="slotProps.data"
+                            icon-name="Trash2"
+                            :route="routes.delete"
+                            @delete-item="onDeleteItem"
+                        />
+                    </div>
+                </template>
+            </Column>
+            <Column
+                field="phone"
+                header="Phone/Email"
+                :pt="{
+                    root: {
+                        class: 'hidden sm:table-cell'
+                    }
+                }"
+            >
+                <template #body="slotProps">
+                    <Button
+                        class="!px-0"
+                        as="a"
+                        variant="link"
+                        :label="slotProps.data.phone"
+                        :href="'tel:' + trimPhone(slotProps.data.phone)"
+                        rel="noopener"
+                    />
+                    <p class="2xl:hidden">
+                        <small class="text-xs font-normal text-gray-900 dark:text-gray-300">{{ slotProps.data.email }}</small>
+                    </p>
+                </template>
+            </Column>
+            <Column
+                field="discount"
+                header="Discount"
+                :sortable="true"
+                :pt="{
+                    root: {
+                        class: 'hidden lg:table-cell'
+                    }
+                }"
+            ></Column>
+            <Column
+                field="total"
+                header="Total"
+                :pt="{
+                    root: {
+                        class: 'hidden lg:table-cell'
+                    }
+                }"
+            ></Column>
+            <Column
+                field="email"
+                header="Email"
+                :pt="{
+                    root: {
+                        class: 'hidden 2xl:table-cell'
+                    }
+                }"
+            ></Column>
+            <Column
+                field="blacklist"
+                header="Blacklist"
+                :sortable="true"
+                :pt="{
+                    root: {
+                        class: 'hidden xl:table-cell'
+                    }
+                }"
+            >
+                <template #body="slotProps">
+                    <Tag :value="slotProps.data.blacklist" :severity="getStatusLabel(slotProps.data.blacklist)" />
+                </template>
+            </Column>
+            <Column
+                field="prepayment"
+                header="Prepayment"
+                :sortable="true"
+                :pt="{
+                    root: {
+                        class: 'hidden xl:table-cell'
+                    }
+                }"
+            >
+                <template #body="slotProps">
+                    <Tag :value="slotProps.data.prepayment" :severity="getStatusLabel(slotProps.data.prepayment)" />
+                </template>
+            </Column>
+            <Column
+                field="comment"
+                header="Comment"
+                :pt="{
+                    root: {
+                        class: 'hidden lg:table-cell'
+                    }
+                }"
+            ></Column>
+            <Column
+                field="source"
+                header="Source"
+                :pt="{
+                    root: {
+                        class: 'hidden 2xl:table-cell'
+                    }
+                }"
+            ></Column>
+            <Column
+                :exportable="false"
+                header="Tools"
+                :pt="{
+                    root: {
+                        class: 'hidden sm:table-cell'
+                    }
+                }"
+            >
+                <template #body="slotProps">
+                    <span class="flex flex-row items-center justify-center">
+                        <UpdateDialog
+                            :key="slotProps.data.id"
+                            v-if="tools.update"
+                            :entity="slotProps.data"
+                            icon-name="Edit2"
+                            label="Edit item"
+                            :route="routes.update"
+                            @update-item="onLoadItem"
+                        />
+                        <DeleteDialog
+                            v-if="tools.remove"
+                            :entity="slotProps.data"
+                            icon-name="Trash2"
+                            :route="routes.delete"
+                            @delete-item="onDeleteItem"
+                        />
+                    </span>
+                </template>
+            </Column>
+        </DataTable>
+    </div>
+</template>
