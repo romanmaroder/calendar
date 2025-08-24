12ced398795b47a5871e4e6f5e76ba335a28ffd7
romanmaroder <kashik.roman@rambler.ru>
2025-08-24 09:43:08 +0300

    Checking for null or undefined

--- resources/js/composables/useFullname.ts
+++ resources/js/composables/useFullname.ts
@@ -4,6 +4,7 @@ export function getFullname(name?: string, middlename?: string, surname?: string
 
     for (let i = 0; i < arguments.length; i++) {
         // eslint-disable-next-line prefer-rest-params
+        if (arguments[i] === undefined || arguments[i] === null) {break;}
         fullname += `${arguments[i]} `;
     }
 
