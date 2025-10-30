3608ddac65104a2941dc10070ce89cfa3f07ac53
romanmaroder <kashik.roman@rambler.ru>
2025-10-31 01:29:38 +0300

    Typo correction

--- resources/js/composables/useFullname.ts
+++ resources/js/composables/useFullname.ts
@@ -9,7 +9,7 @@ export function getFullname(data: Data): string {
     const middlename = data.middlename ? data.middlename[0]?.toUpperCase() + data.middlename?.slice(1).toLowerCase() : '';
     const surname = data.surname ? data.surname[0]?.toUpperCase() + data.surname?.slice(1).toLowerCase() : '';
 
-    if (name.length !== 0 || middlename.length !== 0 || middlename.length !== 0) {
+    if (name.length !== 0 || middlename.length !== 0 || surname.length !== 0) {
         return `${name} ${middlename} ${surname}`;
     }
     return '';
