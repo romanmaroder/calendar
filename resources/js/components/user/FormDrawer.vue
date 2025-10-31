d3cda3c2612561688c79bc239a78f6db8e6181ef
romanmaroder <kashik.roman@rambler.ru>
2025-10-31 21:49:33 +0300

    Form for adding editing. Wrapper for displaying form in sidebar

---
+++ resources/js/components/user/FormDrawer.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import FormUser from '@/components/user/FormUser.vue';
+import { getFullname } from '@/composables/useFullname';
+import { ref } from 'vue';
+
+interface Data {
+    name?: string;
+    surname?: string;
+    avatar?: string;
+}
+const emit = defineEmits(['newUser','updateUser']);
+
+defineProps({
+    iconName: {
+        type: String,
+        default: '',
+    },
+    label: {
+        type: String,
+        default: 'Add',
+    },
+    title: {
+        type: String,
+        default: 'New',
+    },
+    entity:{
+        type:Object,
+        default(){
+            return {};
+        }
+    },
+    variant: {
+        type: String,
+        validator(value:string) {
+            return [ "outlined", "text" , "link" ].includes(value)
+        },
+        default:''
+    },
+});
+
+const visible = ref(false);
+const form= ref<Data>({});
+
+const previewOfUserData = (data: Data) => {
+    return form.value={
+        name: data.name,
+        surname: data.surname,
+        avatar: data.avatar,
+    };
+};
+const newUser = ()=>{emit('newUser'); visible.value = false;};
+const updateUser = ()=>{emit('updateUser'); visible.value = false;};
+
+
+</script>
+
+<template>
+    <Drawer
+        v-model:visible="visible"
+        :blockScroll="true"
+        :position="entity.id ? 'right' : undefined "
+        :closeIcon="entity.id ? 'pi pi-chevron-right' : 'pi pi-chevron-left' "
+        :pt="{
+            header: {
+                class: '!py-[0.5rem]'
+            },
+            title: {
+                class: '!text-lg'
+            }
+        }"
+    >
+        <template #header>
+            <div class="flex items-center gap-2">
+                <Avatar
+                    v-if="form.avatar || entity.avatar"
+                    :image="form.avatar ?? entity.avatar"
+                    shape="circle"
+                    :pt="{
+                        image: {
+                            class: 'object-cover',
+                        },
+                    }"
+                />
+                <Avatar v-else icon="pi pi-user" shape="circle" />
+                <span v-if="form.name || form.surname" class="inline-block w-[150px] truncate font-bold">
+                    {{ getFullname({ name: form.name, surname: form.surname }) }} </span
+                >
+                <span v-else-if="entity.name || entity.surname" class="inline-block w-[150px] truncate font-bold">
+                    {{ getFullname({ name: entity.name, surname: entity.surname }) }} </span
+                >
+                <span v-else class="line-clamp-1 inline-block w-[150px] font-bold">{{ title }}</span>
+            </div>
+        </template>
+        <FormUser @previewOfUserData="previewOfUserData"
+                  @createItem="newUser"
+                  @updateItem="updateUser"
+                  :user="entity"
+        />
+    </Drawer>
+
+    <Button :icon="iconName" :label="label" @click="visible = true" size="small" :variant="variant" />
+</template>
---
+++ resources/js/components/user/FormUser.vue
@@ -0,0 +1,256 @@
+<script setup lang="ts">
+import InputError from '@/components/InputError.vue';
+import { useLabelName } from '@/composables/useLabelName';
+import { useForm } from '@inertiajs/vue3';
+import { useToast } from 'primevue/usetoast';
+import { inject, ref, watch } from 'vue';
+import AvatarUpload from '@/components/AvatarUpload.vue';
+import { getFullname } from '@/composables/useFullname';
+
+const branches: any = inject('listOfBranches');
+const routes: any = inject('formRoutesProps');
+
+const props = defineProps({
+    user: {
+        type: Object,
+        default() {
+            return {};
+        },
+    },
+});
+
+const emit = defineEmits(['createItem', 'previewOfUserData', 'updateItem']);
+const wait = (time = 1000) => new Promise((resolve) => setTimeout(resolve, time));
+const open = ref(false);
+const toast = useToast();
+
+const form = useForm({
+    id: props.user.id ?? '',
+    avatar: props.user.avatar ?? '',
+    name: props.user.name ?? '',
+    surname: props.user.surname ?? '',
+    middleName: props.user.middleName ?? '',
+    phone: props.user.phone ?? '',
+    email: props.user.email ?? '',
+    branch_id: props.user.branch_id ?? '',
+    birthday: props.user.birthday ?? '',
+    comment: props.user.comment ?? '',
+    created_at: props.user.created_at ?? '',
+});
+
+const submit = (e: Event) => {
+    e.preventDefault();
+    if (form.id) {
+        form.put(route(`${routes.value.update}`, { user: form.data() }), {
+            preserveScroll: true,
+            onSuccess: function () {
+                wait().then(() => (open.value = false));
+                toast.add({
+                    severity: 'info',
+                    summary: 'Info',
+                    detail: form.name + ' - update successfully.',
+                    life: 3000,
+                });
+                closeModal();
+                emit('updateItem');
+            },
+            onError: function (errors) {
+                toast.add({
+                    severity: 'error',
+                    summary: 'Validation Error',
+                    detail: showErrors(errors),
+                    life: 2000,
+                });
+            },
+        });
+    } else {
+        form.post(route(routes.value.create), {
+            preserveScroll: true,
+            onSuccess: function () {
+                wait().then(() => (open.value = false));
+                toast.add({
+                    severity: 'info',
+                    summary: 'Info',
+                    detail: form.name + ' ' + form.surname + ' - add successfully.',
+                    life: 3000,
+                });
+                closeModal();
+                emit('createItem');
+            },
+            onFinish: function () {
+                form.reset();
+            },
+            onError: function (errors) {
+                toast.add({
+                    severity: 'error',
+                    summary: 'Validation Error',
+                    detail: showErrors(errors),
+                    life: 2000,
+                });
+                form.defaults();
+            },
+        });
+    }
+};
+
+const showErrors = (errors: any) => {
+    let message = '';
+    for (const key in errors) {
+        message += `${errors[key]}` + '\n';
+    }
+    return message;
+};
+
+const closeModal = () => {
+    form.clearErrors();
+    form.reset();
+};
+
+const { dateLabelName, selectLabelName } = useLabelName();
+
+const setDate = (date: any): void => {
+    form.birthday = dateLabelName(date);
+};
+
+const onUpdateAvatar = (data: any) => {
+    form.avatar = data.src;
+    toast.add({ severity: 'info', summary: 'Info', detail: data.message, life: 3000 });
+};
+
+/*onMounted(()=>{
+})*/
+
+watch(form, () => {
+    emit('previewOfUserData', { name: form.name, surname: form.surname, avatar: form.avatar });
+});
+</script>
+
+<template>
+    <form class="flex h-full flex-1 flex-col gap-4 rounded-xl sm:flex-row sm:p-4 dark:bg-[#18181B]">
+        <div class="md:border-sidebar-border/70 dark:sm:border-sidebar-border relative hidden flex-none rounded-xl sm:block sm:w-56">
+            <AvatarUpload :url="routes.upload" orientation="landscape" @updateAvatar="onUpdateAvatar" preview img-position="top" />
+        </div>
+        <div class="grid flex-1 gap-4 sm:grid-cols-2 md:grid-flow-row-dense md:grid-cols-2 md:grid-rows-3">
+            <div
+                class="md:border-sidebar-border/70 dark:sm:border-sidebar-border relative grid content-start gap-3 rounded-xl p-2 sm:col-span-1 sm:row-span-1 sm:border md:col-span-1"
+            >
+                <FloatLabel variant="on" class="">
+                    <InputText
+                        id="name"
+                        v-model="form.name"
+                        autocomplete="off"
+                        class="w-full"
+                        aria-labelledby="name"
+                        size="small"
+                        v-keyfilter="/^[A-zА-яёЁ\s]+$/iu"
+                    />
+                    <label for="name" class="font-light!">{{ getFullname({ name: form.name }) || 'Имя:' }}</label>
+                </FloatLabel>
+                <InputError :message="form.errors.name" class="-mt-2 mb-2" />
+                <FloatLabel variant="on" class="">
+                    <InputText
+                        id="middleName"
+                        v-model="form.middleName"
+                        autocomplete="off"
+                        class="w-full"
+                        aria-labelledby="middleName"
+                        size="small"
+                        v-keyfilter="/^[A-zА-яёЁ\s]+$/iu"
+                    />
+                    <label for="middleName" class="font-light!">{{ getFullname({ middlename: form.middleName }) || 'Отчество:' }}</label>
+                </FloatLabel>
+                <InputError :message="form.errors.middleName" class="-mt-2 mb-2" />
+                <FloatLabel variant="on" class="">
+                    <InputText
+                        id="surname"
+                        v-model="form.surname"
+                        autocomplete="off"
+                        class="w-full"
+                        aria-labelledby="surname"
+                        size="small"
+                        v-keyfilter="/^[A-zА-яёЁ\s]+$/iu"
+                    />
+                    <label for="surname" class="font-light!">{{ getFullname({ surname: form.surname }) || 'Фамилия:' }}</label>
+                </FloatLabel>
+                <InputError :message="form.errors.surname" class="-mt-2 mb-2" />
+                <FloatLabel variant="on" class="">
+                    <Select
+                        v-model="form.branch_id"
+                        id="branch"
+                        optionLabel="name"
+                        :options="branches"
+                        option-value="id"
+                        class="w-full"
+                        aria-labelledby="branch"
+                        size="small"
+                        fluid
+                    />
+                    <label for="branch" class="font-light!">{{ selectLabelName(branches, form.branch_id) || 'Филиал' }}</label>
+                </FloatLabel>
+                <InputError :message="form.errors.branch_id" class="-mt-2 mb-2" />
+            </div>
+            <div
+                class="sm:border-sidebar-border/70 dark:sm:border-sidebar-border relative grid content-start gap-3 overflow-hidden rounded-xl p-2 md:col-span-1 md:row-span-1 md:border"
+            >
+                <FloatLabel variant="on">
+                    <InputMask
+                        id="phone"
+                        v-model="form.phone"
+                        type="tel"
+                        class="w-full"
+                        aria-labelledby="phone"
+                        size="small"
+                        mask="+9 999 999 99 99"
+                    />
+                    <label for="phone" class="font-light!">{{ form.phone || '+9 999 999 99 99' }}</label>
+                </FloatLabel>
+                <InputError :message="form.errors.phone" class="-mt-2 mb-2" />
+
+                <FloatLabel variant="on" class="">
+                    <InputText id="email" v-model="form.email" type="email" autocomplete="off" class="w-full" aria-labelledby="email" size="small" />
+                    <label for="email" class="font-light!">Email:</label>
+                </FloatLabel>
+                <InputError :message="form.errors.email" class="-mt-2 mb-2" />
+                <FloatLabel variant="on">
+                    <DatePicker
+                        :v-model="form.birthday"
+                        dateFormat="yy-mm-dd"
+                        id="birthday"
+                        name="birthday"
+                        showIcon
+                        showButtonBar
+                        iconDisplay="input"
+                        size="small"
+                        class="w-full"
+                        aria-labelledby="birthday"
+                        @update:model-value="setDate"
+                    />
+                    <label class="font-light!" for="birthday">{{ form.birthday || 'ДР' }}</label>
+                </FloatLabel>
+                <InputError :message="form.errors.birthday" class="-mt-2 mb-2" />
+            </div>
+            <div
+                class="md:border-sidebar-border/70 dark:sm:border-sidebar-border relative grid content-start overflow-hidden rounded-xl p-2 sm:col-span-2 sm:border md:col-span-2 md:row-span-1"
+            >
+                <FloatLabel variant="on">
+                    <Textarea id="comment" v-model="form.comment" rows="4" cols="15" autoResize size="small" class="w-full" />
+                    <label class="font-light!" for="comment">Заметка</label>
+                </FloatLabel>
+                <InputError :message="form.errors.comment" class="mt-1 mb-2" />
+            </div>
+            <div
+                class="md:border-sidebar-border/70 dark:sm:border-sidebar-border relative grid content-start gap-2 overflow-hidden rounded-xl p-2 sm:col-span-2 sm:hidden md:col-span-2 md:row-span-1 md:border"
+            >
+                <AvatarUpload :url="routes.upload" orientation="landscape" @updateAvatar="onUpdateAvatar" preview img-position="top" />
+            </div>
+            <div
+                class="relative grid grid-flow-row content-evenly gap-2 overflow-hidden rounded-xl p-2 sm:col-span-2 sm:row-span-1 sm:grid-flow-col sm:self-start md:justify-self-end"
+            >
+                <Button :disabled="form.processing" class="cursor-pointer" @click="submit" raised>
+                    {{ form.processing ? 'Сохранение...' : 'Сохранить' }}
+                </Button>
+                <Button severity="secondary" @click="closeModal" class="cursor-pointer" raised> Отмена</Button>
+            </div>
+        </div>
+    </form>
+</template>
