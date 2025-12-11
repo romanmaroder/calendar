6066954cd98241c7a8da50c33f423e986edeed69
romanmaroder <kashik.roman@rambler.ru>
2025-12-11 15:45:51 +0300

    Branch Provider for Form

--- resources/js/pages/user/Edit.vue
+++ resources/js/pages/user/Edit.vue
@@ -3,19 +3,24 @@ import FormUser from '@/components/user/FormUser.vue';
 import Layout from '@/layouts/AppLayout.vue';
 import { BreadcrumbItem, User } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import { PropType } from 'vue';
+import { PropType, provide, ref } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     { title: 'Users', href: '/users' },
     { title: 'Update user', href: '' },
 ];
 
-defineProps({
+const props =defineProps({
     user: Object as PropType<User | null>,
     branch: {
         type: Array,
     }
 });
+
+// Предоставляем (provide) listOfBranches всем дочерним компонента список филиалов
+const listOfBranches: object = ref(props.branch);
+provide('listOfBranches', listOfBranches);
+
 </script>
 
 <template>
