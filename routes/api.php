760206b1fa99286eb664cefe3ec374839e5ab108
romanmaroder <kashik.roman@rambler.ru>
2025-12-11 14:00:56 +0300

    Transferring routes for working with an avatar

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Upload\AvatarUploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +7,3 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/upload',[AvatarUploadController::class,'store'] );
-Route::post('/delete',[AvatarUploadController::class,'delete'] );
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use App\Http\Controllers\Upload\AvatarUploadController;
+use App\Http\Controllers\Upload\AvatarController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -12,6 +14,11 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::post('/upload',[AvatarUploadController::class,'store'] )->name('upload');
+Route::post('/delete',[AvatarUploadController::class,'destroy'] )->name('delete');
+
+Route::post('/api/upload', [AvatarController::class, 'store'])->name('store');
+Route::delete('/api/destroy', [AvatarController::class, 'destroy'])->name('destroy');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
