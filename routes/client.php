b4042d1bca5ee47b4626396b23764921f21ff42a
romanmaroder <kashik.roman@rambler.ru>
2025-12-11 20:21:28 +0300

    New route

--- routes/client.php
+++ routes/client.php
@@ -5,18 +5,23 @@ use App\Http\Controllers\Client\ClientController;
 Route::middleware(['auth'])->group(function () {
     Route::redirect('clients', '/clients/index');
 
-    Route::get('archive', [ClientController::class, 'archive'])->name('archive');
-    Route::get('clients/index', [ClientController::class, 'index'])->name('clients.index');
-    Route::post('clients/index', [ClientController::class, 'store'])->name('clients.store');
-    Route::get('clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-    Route::get('clients/{client}', [ClientController::class, 'show'])->name('clients.show');
-    Route::put('clients/{client}', [ClientController::class, 'update'])->name('clients.update');
-    Route::put('clients/restore/{id}',[ClientController::class,'restore'])->name('clients.restore');
-    Route::put('clients/multiRestore/{ids}',[ClientController::class,'multiRestore'])->name('multiRestore');
-    Route::put('/avatar/{client}', [ClientController::class, 'avatar'])->name('avatar');
-    Route::delete('clients/trash/{ids}', [ClientController::class, 'trash'])->name('clients.trash');
-    Route::delete('clients/multiDestroy/{ids}', [ClientController::class, 'multiDestroy'])->name('multiDestroy');
-    Route::delete('clients/destroy/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+    Route::get('clients/archive', [ClientController::class, 'archive'])->name('clients.archive');
+    Route::get('/clients', [ClientController::class, 'index'])->name('clients');
+    Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+    Route::post('/clients/index', [ClientController::class, 'store'])->name('clients.store');
+    Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+    Route::put('/clients/{id}', [ClientController::class, 'update'])->name('clients.update');
+    Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show')->withTrashed();
+    Route::put('/avatar/{client}', [ClientController::class, 'avatar'])->name('clients.avatar');
+    // Soft delete (один/несколько)
+    Route::delete('/clients/bulk/soft', [ClientController::class, 'bulkSoftDelete'])->name('clients.bulk.soft');
+    Route::delete('/clients/{id}', [ClientController::class, 'softDelete'])->name('clients.soft.delete');
+    // Force delete (один/несколько)
+    Route::delete('/clients/bulk/force', [ClientController::class, 'bulkForceDelete'])->name('clients.bulk.force');
+    Route::delete('/clients/{id}/force', [ClientController::class, 'forceDelete'])->name('clients.force');
+    // Restore (один/несколько)
+    Route::post('/clients/bulk/restore', [ClientController::class, 'bulkRestore'])->name('clients.bulk.restore');
+    Route::post('/clients/{id}/restore', [ClientController::class, 'restore'])->name('clients.restore');
 
 });
 
